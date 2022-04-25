a82a880d2d50b5c0106691e448911e8503024b14
Douglas Oliveira <douglas.toliveira@hotmail.com>
2022-04-25 13:15:54 -0300

    User entity and resource

---
+++ src/main/java/com/douglas/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.douglas.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.douglas.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1l, "Douglas", "douglas@gmail.com", "51987654321", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
