edad02aaeb4b689f3c68b9febf1ad27b477fdb4b
Douglas Oliveira <douglas.toliveira@hotmail.com>
2022-04-25 14:21:30 -0300

    Category

---
+++ src/main/java/com/douglas/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.douglas.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.douglas.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
