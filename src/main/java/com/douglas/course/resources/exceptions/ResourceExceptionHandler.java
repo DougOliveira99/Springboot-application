d8acf83f82a9bbd7ee23066cd78ff50069b8d6c8
Douglas Oliveira <douglas.toliveira@hotmail.com>
2022-06-01 01:40:06 -0300

    Exception handling - delete

--- src/main/java/com/douglas/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/douglas/course/resources/exceptions/ResourceExceptionHandler.java
@@ -9,16 +9,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.douglas.course.services.exceptions.DatabaseException;
 import com.douglas.course.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
-	
+
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		String error = "Resource not found.";
 		HttpStatus status = HttpStatus.NOT_FOUND;
-		StandardError stError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		StandardError stError = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(stError);
+	}
+
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> databaseException(DatabaseException e, HttpServletRequest request) {
+		String error = "Database error.";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError stError = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
 		return ResponseEntity.status(status).body(stError);
 	}
 }
